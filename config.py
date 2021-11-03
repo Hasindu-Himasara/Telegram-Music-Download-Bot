c37d7540b4c16e301edf0bdcf444155bea5535de
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-03 15:02:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # TG_MP3_Download_Bot
 
 import os
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("7744764"))
+API_HASH = os.getenv("09fb2bd3ee46019e911149d4970bfc47")
+BOT_TOKEN = os.getenv("2080924626:AAE-_tHwgNDLGPsQIYCoCotpYrrEq_5hmng")
